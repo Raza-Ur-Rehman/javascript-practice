4b1419a0febe3ec97cd748818ded1236eaeddb0b
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-03-13 01:46:34 +0500

    chapter 1 all Question complete
    Alert chapter

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+// chapter 1 //
+
+// Question no 1 
+
 alert("welcome to JS land");
 
-alert("You are Great Coder"+ "\n"+ "Happy Coding!");
+// Question no 2
 
+alert("Error! Enter a invalid input");
 
-console.log("Hello...I can run JS through my web browser's console");
+// Question no 3 
 
+alert("You are Great Coder\nHappy Coding!");
 
+// Question no 4
+
+alert("You are Great Coder")
+
+// Question no 5 
+
+alert("Hello...I can run JS through my web browser's console");
+
+// Question no 6
+
+alert("Using alert in My New Html Project")
