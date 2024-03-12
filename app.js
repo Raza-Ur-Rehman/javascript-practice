d151b72baa6985af58dd239a5f3cc86048b8f49c
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-03-13 02:06:27 +0500

    javascript assigment  still
    chapter 1 and chapter 2 question 1 complete

--- app.js
+++ app.js
@@ -23,3 +23,22 @@ alert("Hello...I can run JS through my web browser's console");
 // Question no 6
 
 alert("Using alert in My New Html Project")
+// chapter 1 closed \\
+
+// chapter 2 //
+
+//  Question no  1
+
+var username;
+
+//  Question no  2
+
+var myName = "Raza";
+
+var lastName = " ur Rehman";
+
+var fullName = myName + lastName;
+
+alert(fullName);
+
+//  Question no  2
