954b5a014ad5629d16040bdc73491c739b7d08be
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-03-18 03:21:01 +0500

    still assigment
    chapter 2 complete

--- app.js
+++ app.js
@@ -1,44 +1,91 @@
-// chapter 1 //
+// // chapter 1 //
 
-// Question no 1 
+// // Question no 1 
 
-alert("welcome to JS land");
+// alert("welcome to JS land");
 
-// Question no 2
+// // Question no 2
 
-alert("Error! Enter a invalid input");
+// alert("Error! Enter a invalid input");
 
-// Question no 3 
+// // Question no 3 
 
-alert("You are Great Coder\nHappy Coding!");
+// alert("You are Great Coder\nHappy Coding!");
 
-// Question no 4
+// // Question no 4
 
-alert("You are Great Coder")
+// alert("You are Great Coder")
 
-// Question no 5 
+// // Question no 5 
 
-alert("Hello...I can run JS through my web browser's console");
+// alert("Hello...I can run JS through my web browser's console");
 
-// Question no 6
+// // Question no 6
 
-alert("Using alert in My New Html Project")
-// chapter 1 closed \\
+// alert("Using alert in My New Html Project")
+// // chapter 1 closed \\
 
 // chapter 2 //
 
-//  Question no  1
+//  Question no  1 to 4
 
 var username;
 
-//  Question no  2
+var myName = "Raza ur Rehman";
 
-var myName = "Raza";
+var message = " Hello world ";
 
-var lastName = " ur Rehman";
+alert(message);
 
-var fullName = myName + lastName;
+var student;
 
-alert(fullName);
+student = "Johne Doe";
 
-//  Question no  2
+alert(student);
+
+student = "15 year old";
+
+alert(student);
+
+student = "Certified Mobile Application Development"
+
+alert(student);
+
+//  Question no  5
+
+var pizza = "pizza \n pizz \n piz \n pi \n p";
+
+alert(pizza);
+
+var email1 = "My email address is ";
+
+var email2 = "mrazaurreheman2@gmail.com";
+
+alert(email1+email2);
+
+var book = "A Smarter way to learn Javascript";
+
+alert(book);
+
+document.write("<h1> I can write HTML contant through Javascript</h1>");
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+alert(design);
+
+// chapter 2 closed//
+
+
+
+// for (let i = 1; i <=10; i++) {
+//     document.write("2 x ",i,"=",2*i,"<br>");
+    
+// }
+
+
+
+
+
+
+
+
